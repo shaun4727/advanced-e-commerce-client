1e2fc104bb36976db4d9d2a921d1272d7fca5a93
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-03-25 06:30:28 +0600

    cookie error fixed

--- app/(adminLayout)/dashboard/agent/dashboard/page.tsx
+++ app/(adminLayout)/dashboard/agent/dashboard/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';

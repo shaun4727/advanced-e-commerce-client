92704e97f3df698e1e32412669664e9b4460d538
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-03-25 07:24:43 +0600

    last login updated

--- app/(adminLayout)/dashboard/agent/dashboard/page.tsx
+++ app/(adminLayout)/dashboard/agent/dashboard/page.tsx
@@ -4,13 +4,13 @@ import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { getProfileDataApi } from '@/services/AuthService';
-import { Clock, Mail, MapPin, Monitor, ShieldCheck } from 'lucide-react';
+import { Mail, MapPin, Monitor, ShieldCheck } from 'lucide-react';
 
 const page = async () => {
     const res = await getProfileDataApi();
     const user = res.data;
 
-    const formattedDate = new Date(user?.lastLogin).toLocaleString();
+    // const formattedDate = new Date(user?.lastLogin).toLocaleString();
     return (
         <Card className=" overflow-hidden border-t-4 border-t-primary shadow-lg">
             <CardHeader className="flex flex-row items-center gap-4 pb-2">
@@ -48,7 +48,7 @@ const page = async () => {
 
                 {/* Status & Security */}
                 <div className="grid grid-cols-2 gap-4 py-2 border-y border-border/50">
-                    <div className="space-y-1">
+                    {/* <div className="space-y-1">
                         <p className="text-[10px] uppercase font-bold text-muted-foreground tracking-wider">
                             Last Login
                         </p>
@@ -56,14 +56,14 @@ const page = async () => {
                             <Clock className="h-3 w-3" />
                             {formattedDate}
                         </div>
-                    </div>
+                    </div> */}
                     <div className="space-y-1">
                         <p className="text-[10px] uppercase font-bold text-muted-foreground tracking-wider">
                             IP Address
                         </p>
                         <div className="flex items-center gap-2 text-xs">
                             <MapPin className="h-3 w-3" />
-                            {user.clientInfo.ipAddress.replace('::ffff:', '')}
+                            {user?.clientInfo?.ipAddress.replace('::ffff:', '')}
                         </div>
                     </div>
                 </div>
@@ -75,7 +75,7 @@ const page = async () => {
                             <Monitor className="h-3.5 w-3.5" /> Client Source
                         </span>
                         <span className="text-muted-foreground italic font-mono">
-                            {user.clientInfo.userAgent}
+                            {user?.clientInfo?.userAgent}
                         </span>
                     </div>
                     <div className="flex items-center justify-between text-xs">
